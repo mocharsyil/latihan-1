eef5c00a2a28c1d1094fbb614ead65778d5443b3
Moch. Arsyil Albany <86009595+mocharsyil@users.noreply.github.com>
2022-03-09 08:15:41 +0700

    menambahkan source code

---
+++ src/main/java/id/ac/uin/arsyil/latihan1/IndexController.java
@@ -0,0 +1,16 @@
+package id.ac.uin.arsyil.latihan1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+
+public class IndexController {
+    @GetMapping("/hello")
+
+    public String hello(){
+        return "Selamat belajar Spring boot!";
+    }
+}
